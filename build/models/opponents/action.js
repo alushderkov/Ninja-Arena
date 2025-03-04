d5098cf3221e2ef9c06966f6a2030e9f8744ae9e
Alexandr Ushakov <alushderkov@gmail.com>
2025-03-04 23:47:32 +0300

    excluded build

--- build/models/opponents/action.js
+++
@@ -1,8 +0,0 @@
-export function Action(arena) {
-    let result;
-    result = arena;
-    result.enemy.health -= 150;
-    result.you.chakra -= 120;
-    result.enemy.chakra += 100;
-    return result;
-}
